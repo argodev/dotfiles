621c965d4cd5ea0e8f5d78770b34910f9d847935
Rob Gillen <argodev@gmail.com>
2024-02-09 19:59:23 -0500

    Working on pre-reqs

---
+++ setup-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+check_preq_exists() {
+  # Test to see git is installed
+  if ! command -v $1 &> /dev/null
+  then
+    echo "$1 is not installed. Installing it now..."
+    sudo apt install -y $1
+  fi
+}
+
+prereqs=("git" "vim" "tmux" "tree")
+
+
+for prereq in ${prereqs[@]}; do
+  check_prereq_exists $prereq
+done
